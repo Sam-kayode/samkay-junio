e04fbbd8bac90f249c6f0635120cca4be09a715a
Sam-kayode <marvelloussamkayode@gmail.com>
2020-10-21 19:16:19 +0100

    new

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="contact" id="top">
-    <div class="pt-5 text-center">
-      <transition name="fade">
-        <div class="logo2" v-if="isSwipe">
-          <img src="../assets/samkay.png" alt="" />
-        </div>
-      </transition>
+        <div class="logo2 text-center" >
+          <img src="../assets/samkay.png" v-if="isSwipe" alt="" />
     </div>
     <h1 class="text-center">Get In Touch</h1>
+    
     <div class="text px-2 px-md-5 pt-5">
       <p class="text-center px-1 px-md-5">
         Lorem ipsum dolor st amet consectetur adipisicing elit. Perferendis
@@ -91,21 +88,27 @@ export default {
 };
 </script>
 <style scoped>
-
+.logo2{
+  height:50px;
+}
 .logo2 img {
-  width: 100px;
+  width: 100px !important;
 }
 .contact {
   background-size: cover;
   background: linear-gradient(rgba(24, 21, 21, 0.7), rgba(22, 20, 20, 0.7)),
-    url("https://res.cloudinary.com/samkay-studios/image/upload/q_auto:low/v1603296618/samkaystudios/camera_aabqmb.jpg")
-      center center;
-  background-size: cover;
+    url("../assets/ll.jpg")   center !important;
+  background-size: cover !important;
   color: white;
   font-size: 19px;
+  padding-top:50px !important;
+overflow-x:hidden;
+max-height:1900px;
 }
 h1 {
   font-family: "Philosopher", sans-serif;
   font-size: 60px;
 }
+
+
 </style>
